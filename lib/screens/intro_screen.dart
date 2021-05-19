8617ae57e6192ec90deb2068034e7009f2c56584
Mohammedamin Sultan Abdullah <mamibubu999@gmail.com>
2021-05-19 14:42:59 +0300

    initial

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intro_screen_onboarding_flutter/intro_app.dart';
 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Premier League')
+      ),
+      body: ,
+    );
   }
 }
